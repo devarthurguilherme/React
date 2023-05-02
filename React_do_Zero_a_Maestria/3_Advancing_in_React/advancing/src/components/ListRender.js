57dec3093d357a85241a651c27f15cdf33417d94
unknown <arthurguilherme2013@gmail.com>
2023-05-02 05:24:06 +0100

    Studying Previous State

--- React_do_Zero_a_Maestria/3_Advancing_in_React/advancing/src/components/ListRender.js
+++ React_do_Zero_a_Maestria/3_Advancing_in_React/advancing/src/components/ListRender.js
@@ -2,40 +2,39 @@ import {useState} from 'react';
 
 const ListRender = () => {
 
-    const [List] = useState([
-        "Arthur", "Thávea", "Alexander Jr", "Léa"
-    ]);
+   const [list] = useState(["Arthur", "Thávia", "Alexander JR", "Léa", "Nathan"]);
 
-    const [List2] = useState([
-        {key: 1, nome: "Arthur Guilherme", age: 31, profission: "Web Developer"},
-        {key: 2, nome: "Thave Santos", age: 27, profission: "Ecommerce Manager"},
-        {key: 3, nome: "Alexander JR", age: 25, profission: "Mechanic"},
-        {key: 4, nome: "Léa", age: 23, profission: "Wife House"}
-    ]);
+   const [users, setUsers] = useState([
+    {id: 1, nome: "Arthur", age: 31, profission: "Developer"},
+    {id: 2, nome: "Thávia", age: 27, profission: "Manager"},
+    {id: 3, nome: "Jr", age: 25, profission: "Mecanic"},
+    {id: 4, nome: "Léa", age: 23, profission: "Wife House"}
+   ]);
+
+   const deleteRandom = () => {
+        const randomNumber = Math.floor(Math.random() * users.length);
+
+        setUsers((prevUsers) => {
+            console.log(prevUsers)
+            return prevUsers.filter((user) => randomNumber !== user.id)
+        })
+   }
 
     return (
         <div>
             <h2>List Render here!</h2>
-            <h3>List without Key</h3>
             <ul>
-                {List.map(
-                    (n, i) => (
-                        <li key={i}>
-                            {"Each leaner -> " + n}
-                        </li>
-                    )
-                )}
+                {list.map((item, i) => (
+                    <li key={i}>{item}</li>
+                ))}
             </ul>
-            <h3>List with key</h3>
+            <h3>List Render with single key</h3>
             <ul>
-                {List2.map((n2) => (
-                        <li key={List2.key}>
-                            {n2.nome} - {n2.age} - {n2.profission} 
-                        </li>
-                    ))
-                }
+                {users.map((user) => (
+                    <li key={user.id}>{user.nome} - {user.age} - {user.profission}</li>
+                ))}
             </ul>
-
+            <button onClick={deleteRandom}>Delete Random User</button>            
         </div>
     );
 };
