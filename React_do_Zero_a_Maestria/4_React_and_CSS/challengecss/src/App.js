7bff16a2665491fdefed501329a3c51b3da29e73
unknown <arthurguilherme2013@gmail.com>
2023-05-25 12:51:01 +0100

    The Begins of the Form with React

--- React_do_Zero_a_Maestria/4_React_and_CSS/challengecss/src/App.js
+++ React_do_Zero_a_Maestria/4_React_and_CSS/challengecss/src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
 //Components
-import CarDetails from './components/CardDetails'
+import CarDetails from './components/CarDetails'
 
 function App() {
+  const cars = [
+    {id: 1, brand: "Fiat", km: 278000, color: "Gray", year: 1994},
+    {id: 2, brand: "Hyundai", km: 170000, color: "Black", year: 2005},
+    {id: 3, brand: "BMW", km: 108000, color: "Blue", year: 2017},
+    {id: 4, brand: "Iveco", km: 178000, color: "Blue", year: 2018}
+  ];
+
   return (
     <div className="App">
       <h1>Challenge CSS</h1>
-      <CarDetails />
+      {cars.map((car) => (
+        <CarDetails 
+          brand={car.brand}
+          km={car.km}
+          color={car.color}
+          year={car.year}
+        />
+      ))}
     </div>
   );
 }
--- React_do_Zero_a_Maestria/4_React_and_CSS/challengecss/src/components/CardDetails.js
+++
@@ -1,9 +0,0 @@
-const CarDetails = () => {
-    return (
-        <div>
-            <h2>Car Details here</h2>
-        </div>
-    );
-};
-
-export default CarDetails;
