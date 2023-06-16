08291ccf7dda5317d03df3ff25ccde976788a80c
unknown <arthurguilherme2013@gmail.com>
2023-06-16 12:04:53 +0100

    Studying ways to rescue data of the API

--- React_Project_Form/project-form/src/components/MyForm.js
+++ React_Project_Form/project-form/src/components/MyForm.js
@@ -1,15 +1,43 @@
 import './MyForm.css'
+import {useState} from 'react';
 
 const MyForm = () => {
+
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Enviando o Formulário . . .");
+        console.log(name, email);
+    }
+
+
+    
+
     return (
         <div className="container__form">
-            <form action="">
+            <form onSubmit={handleSubmit}>
                 <div className="form__input-box">
                     <label htmlFor="user-name">Name:</label>
-                    <input type="text" name="user-name" placeholder="name" />
+                    <input 
+                        type="text" 
+                        name="user-name" 
+                        placeholder="digite seu nome"
+                        onChange={(e) => {setName(e.target.value)}} />
                 </div>
                 <div className="form__input-box">
-                    <input type="submit" value="Submit" />
+                    <label>
+                        <span>e-mail</span>
+                        <input 
+                            type="email" 
+                            placeholder="digite seu email"
+                            onChange={(e) => {setEmail(e.target.value)}}
+                            />
+                    </label>
+                </div>
+                <div className="form__input-box">
+                    <input type="submit" value="enviar" />
                 </div>
             </form>
         </div>
---
+++ React_do_Zero_a_Maestria/7_Http_Request/httprequest/src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+
+import { useState, useEffect } from 'react';
+
+const url = "http://localhost:3000/products";
+
+function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+
+    async function fetchData() {
+      const res = await fetch(url);
+      const data = await res.json();
+      setProducts(data);
+    }
+
+    fetchData();
+
+  }, []);
+
+  console.log(products);
+
+  return (
+    <div className="App">
+      <h1>Products List</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.name} $:{product.price}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
