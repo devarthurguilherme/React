4be736f244b3ad5ab076279d20e1da5e3b4aa92e
unknown <arthurguilherme2013@gmail.com>
2023-06-19 13:20:25 +0100

    Studying about sending data to backend and dinamic loading of data

--- React_do_Zero_a_Maestria/7_Http_Request/httprequest/src/App.js
+++ React_do_Zero_a_Maestria/7_Http_Request/httprequest/src/App.js
@@ -7,19 +7,47 @@ const url = "http://localhost:3000/products";
 function App() {
   const [products, setProducts] = useState([]);
 
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+
   useEffect(() => {
 
     async function fetchData() {
       const res = await fetch(url);
       const data = await res.json();
       setProducts(data);
+
     }
 
     fetchData();
 
   }, []);
 
-  console.log(products);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const product = {
+      name,
+      price
+    }
+
+    const res = await fetch(url, {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(product),
+    });
+
+    //Dinamic Loading
+    const addedProducts = await res.json()
+
+    setProducts((prevProducts) => [...prevProducts, addedProducts]);
+
+    console.log(product);
+    setName("");
+    setPrice("");
+  }
 
   return (
     <div className="App">
@@ -29,6 +57,19 @@ function App() {
           <li key={product.id}>{product.name} $:{product.price}</li>
         ))}
       </ul>
+      <div className="Form-Box">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Name:
+            <input type="text" value={name} name="name" onChange={(e) => setName(e.target.value)} />
+          </label>
+           <label>
+            Price:
+            <input type="number" value={price} name="price" onChange={(e) => setPrice(e.target.value)} />
+          </label>
+          <input type="submit" value="To Create" />
+        </form>
+      </div>
     </div>
   );
 }
