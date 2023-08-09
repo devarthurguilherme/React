8bf6938dea310954acc8c4cd596d3768b39dd56e
unknown <arthurguilherme2013@gmail.com>
2023-08-09 11:43:34 +0100

    Course Ultimate React: Studying about: Handling Events and UseState

--- Ultimate_React_Course/1_Creating_React_App/react-app/src/components/ListGroup.tsx
+++ Ultimate_React_Course/1_Creating_React_App/react-app/src/components/ListGroup.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function ListGroup() {
 
     let items = [
@@ -7,14 +9,24 @@ function ListGroup() {
         "Paris",
         "Canada"
     ];
-
+    
+    const [selectedIndex, setSelectedIndex] = useState(-1);
 
     return (
         <>
             <h1>List</h1>
             {items.length === 0 && <p>No item found!</p>}
             <ul className="list-group">
-                {items.map(item => (<li key={item} className="list-group-item">{item}</li>))}
+                {items.map((item, index) => (
+                <li 
+                    key={item} 
+                    onClick={() => {setSelectedIndex(index);}} 
+                    className={ 
+                        selectedIndex === index ? "list-group-item active" : "list-group-item"
+                    }>
+                        {item}
+                    </li>
+                ))}
             </ul>
         </>
     );
