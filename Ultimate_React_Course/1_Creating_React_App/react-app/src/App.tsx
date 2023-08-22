4cd8a0ada71f3b398896a90b8f7dfeee8d34d9f2
unknown <arthurguilherme2013@gmail.com>
2023-08-22 11:07:50 +0100

    Exercise about changing component, using conditional render

--- Ultimate_React_Course/1_Creating_React_App/react-app/src/App.tsx
+++ Ultimate_React_Course/1_Creating_React_App/react-app/src/App.tsx
@@ -1,10 +1,24 @@
-import ListGroup from "./components/ListGroup/";
+import { useState } from "react";
 
 function App () {
-  
+  const [isVisible, setVisible] = useState(false);
+  const [initial, setInitial] = useState(0);
+
+  const handleVisibility = () => {
+    setVisible(true);
+    console.log(isVisible);
+    setInitial((initial) + 1);
+  };
+
+  const handleReset = () => {
+    setInitial(0);
+  }
+
   return (
     <div>
-      <ListGroup />
+      <button onClick={handleVisibility}>Button</button>
+      <button  onClick={handleReset}>reset paragraph</button>
+      <p>{initial}</p>
     </div>
   )
 };
---
+++ Ultimate_React_Course/1_Creating_React_App/react-app/src/components/Button/Button.tsx
@@ -0,0 +1,12 @@
+import styles from"./Button.module.css";
+
+  
+const Button = () => {
+  return (
+    <div>
+        <button>Button</button>
+    </div>
+  );
+};
+
+export default Button
